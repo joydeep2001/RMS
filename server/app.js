81bc464a2eac6744dd50341e94ee853bd3b16d1c
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2021-08-25 01:27:24 +0530

    auth route created

--- server/app.js
+++ server/app.js
@@ -1,48 +1,10 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(express.json());
 
-const users = [];
-app.get('/', (req, res) => {
-    res.json(users);
-});
-app.post('/signup', async (req, res) => {
-    try{
-        const salt = await bcrypt.genSalt(10);
-        const hashedpass =await  bcrypt.hash(req.body.password, salt);
-        const user = {
-            id: req.body.id,
-            password: hashedpass
-        };
-        users.push(user);
-        res.status(201).send();
-    } catch(err) {
-        res.status(500).send();
-        console.log(err);
-    }
-    
-});
-app.post('/login',async (req, res) => {
-    
-    const user =  users.find(user => user.id === req.body.id);
-    console.log(user);
-    if(!user) {
-        return res.status(400).send('User not found');
-    }
-    try {
-        let found = await bcrypt.compare(req.body.password, user.password);
-        if(found) {
-            res.status(200).send("Login success");
-        } else {
-            res.status(400).send("Userid password does not match");
-        }
-    
-    } catch(err) {
-        console.log(err);
-        res.status(500).send();
-    }
+app.use("/auth", authRoutes);
 
-});
 app.listen(3001);
